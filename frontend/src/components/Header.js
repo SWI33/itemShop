ff054a7a4202a49a4223b4839c718e241c8abda2
tyler belmonte <tylerbelmonte@gmail.com>
2021-04-12 17:44:13 -0500

    Second Commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Item-Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">
+              <i class="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link href="#link">
+              <i className="fas fa-shopping-cart"></i>Cart
+            </Nav.Link>
+            <Nav.Link href="#link">
+              {" "}
+              <i className="fas fa-user"></i>Sign In
+            </Nav.Link>
+          </Nav>
+          <Form inline>
+            <NavDropdown title="Admin" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
